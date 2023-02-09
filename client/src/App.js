d4d17b5e7db6c9586f1627822d63579422674ea8
Sanjaya Paudel <paudelsanjaya702@gmail.com>
2023-02-09 16:33:55 +0545

    page outlet management

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-import Post from "./Post";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
-import Login from "./Login";
-import Register from "./Register";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import IndexPage from "./pages/IndexPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Post />} />
+        <Route index element={<IndexPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Route>
