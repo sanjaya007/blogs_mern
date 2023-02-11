feafcf1895bc4f84e8067a6b3be51df1a214b427
Sanjaya Paudel <paudelsanjaya702@gmail.com>
2023-02-11 12:40:33 +0545

    login api created

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -26,7 +26,32 @@ const userRegister = async (req, res) => {
 };
 
 const userLogin = async (req, res) => {
+  const { username, password } = req.body;
   try {
+    const user = await UserModal.findOne({ username });
+
+    if (!user) {
+      res.json({
+        success: false,
+        message: "Username or password is incorrect !",
+      });
+      return false;
+    }
+
+    const result = user.comparePassword(password);
+
+    if (!result) {
+      res.json({
+        success: false,
+        message: "Email or password is incorrect !",
+      });
+      return false;
+    }
+
+    res.json({
+      success: true,
+      message: "Login successful !",
+    });
   } catch (error) {
     console.log(error);
     res.send(400).json(error);
